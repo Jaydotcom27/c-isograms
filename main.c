55de271d0bbaea79b7952951125c3f1f20ef8a12
Jeremy <jeremy-alejandro_28@hotmail.com>
2020-12-09 22:36:09 +0000

    initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Jay
+ *
+ * Created on 9 de diciembre de 2020, 06:31 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
