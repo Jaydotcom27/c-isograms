4619423cfd12c1086e75f3a297f0bb4291961dd3
Jeremy <jeremy-alejandro_28@hotmail.com>
2020-12-09 23:18:02 +0000

    passed sce-1-1-2

--- tests/newsimpletest1.c
+++ tests/newsimpletest1.c
@@ -16,6 +16,14 @@ void TestIsIsogram(){
     }
 }
 
+void TestIsIsogram2(){
+    char arg[] = "jeremy";
+    bool result = IsIsogram(arg); 
+    if (result != 0 ){
+        printf("%%TEST_FAILED%% time=0 testname=TestIsIsogram2 (newsimpletest1) message=Is not isogram.\n", arg);
+    }
+}
+
 int main(int argc, char** argv) {
     printf("%%SUITE_STARTING%% newsimpletest1\n");
     printf("%%SUITE_STARTED%%\n");
@@ -24,6 +32,10 @@ int main(int argc, char** argv) {
     TestIsIsogram();
     printf("%%TEST_FINISHED%% time=0 TestIsIsogram (newsimpletest1) \n");
 
+    printf("%%TEST_STARTED%% TestIsIsogram2 (newsimpletest1)\n");
+    TestIsIsogram();
+    printf("%%TEST_FINISHED%% time=0 TestIsIsogram2 (newsimpletest1) \n");
+    
     printf("%%SUITE_FINISHED%% time=0\n");
 
     return (EXIT_SUCCESS);
